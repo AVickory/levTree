c0cf81ddfd7364f195f9db2200af1bdca631b7a8
AVickory <andrew.vickory@gmail.com>
2016-03-13 13:17:13 -0700

    updated tests for tree (some debugging in tree as well)

--- tree.go
+++ tree.go
@@ -1,58 +1,55 @@
 package levTree
+
 /*
 The Tree module sets up the data structures required to represent tree nodes in
-memory.  The basic building block is the record which consists of a location
+memory.  The basic building block is the Record which consists of a location
 and some data (location basically being a key in the database).
 
-Nodes contain a record with thier own location and the data that's meant
-to be stored on that node.  They also contain a parent record which contains
+Nodes contain a Record with thier own location and the data that's meant
+to be stored on that Node.  They also contain a parent Record which contains
 thier parent's location and some data describing thier parent.  Additionally
-they have a record for each of that node's children.
+they have a Record for each of that Node's children.
 
-Within the provided API, note that a locateable can be either a record or a 
-node.  This simplifies the api so that calling .UpdateNodeData on a node's
-parent record is the same thing as calling it on the parent.
+Within the provided API, note that a locateable can be either a Record or a
+Node.  This simplifies the api so that calling .UpdateNodeData on a Node's
+parent Record is the same thing as calling it on the parent.
 
 Within the database there are four kinds of nodes.  They are not different
 types, again to simplify the API, but rather designate how thier children will
 be bucketed relative to themselves.
 
-Branch - A "normal" node.  Children of this kind of node will be in the same bucket
-that this node is.  Most nodes should be Branches since other types will cause
+Branch - A "normal" Node.  Children of this kind of Node will be in the same bucket
+that this Node is.  Most nodes should be Branches since other types will cause
 the keys in the database to get progressively longer.  a single branch should
 generally hold all of the data that you'll need on a given db access (within
 reason; leveldb will run slowly if your entries get to long).
 
-Tree - The root node of a single tree.  A tree is a node who's children will use the
+Tree - The root Node of a single tree.  A tree is a Node who's children will use the
 tree's key as thier bucket.  They allow for grouping of related entries in the
 db and allow for sequential access of all of thier descendants at once.  Trees
 are generally meant to be the children of either other trees or forests.
 
-Forest - A tree node that is attached to the root node of the db.  If you're
-coming from a SQL background, then you might think about forests as your 
+Forest - A tree Node that is attached to the root Node of the db.  If you're
+coming from a SQL background, then you might think about forests as your
 database's tables and a forest's child trees as sub tables.
 
-Root - the bottom namespace of the database.  This node is just a place to hold
+Root - the bottom namespace of the database.  This Node is just a place to hold
 Meta data about your forests.  Since you can't directly name your forests, you
 can instead put identifying data in the root's child metadata records.
 */
 
 import (
-	"encoding/gob"
-	"bytes"
 	"fmt"
 )
 
-
-
-//this type is just a lexical device to make it clear when an argument 
+//this type is just a lexical device to make it clear when an argument
 //represents something that will be passed to an updater's .Update method.
 type updateData interface{}
 
 //Updates are assumed to be commutative.  Note that because of the interface
 //type of the argument, this method will have to coerce it's argument.
 type updater interface {
-	Update(updateData) error
+	Update(updateData) (updater, error)
 }
 
 //allows records and nodes to be handled by the same methods.  This ends up
@@ -62,209 +59,185 @@ type updater interface {
 type locateable interface {
 	Key() []byte
 	KeyString() string
-	Update(updateData) error
+	Update(updateData) (Record, error)
 	GetData() updater
 }
 
-//a record describes a location in the db.
-type record struct {
-	Loc location
+//a Record describes a location in the db.
+type Record struct {
+	Loc  location
 	Data updater
 }
 
-func (r *record) Key() []byte {
+func (r Record) Key() []byte {
 	return r.Loc.Key()
 }
-func (r *record) KeyString() string {
+func (r Record) KeyString() string {
 	return r.Loc.KeyString()
 }
-func (r *record) Update(u updateData) error {
-	return r.Data.Update(u)
+func (r Record) Update(u updateData) (Record, error) {
+	Data, err := r.Data.Update(u)
+	r.Data = Data
+	if err != nil {
+		fmt.Println("error updating record")
+		return r, err
+	}
+	return r, nil
 }
-func (r *record) GetData() updater {
+func (r Record) GetData() updater {
 	return r.Data
 }
 
-//one tree node.  It is itself a record and contains records, but it's
-//important to note that the record objects in the parent and children fields
+
+//one tree Node.  It is itself a Record and contains records, but it's
+//important to note that the Record objects in the parent and children fields
 //do not necesarily contain the same data that is at the corresponding
 //locations in the db, but rather data describing the connection between those
 //locations and this one.  this is meant to allow a user to traverse the tree
 //without loading more nodes from the db than necesary.
-type node struct {
-	*record
+type Node struct {
+	Record
 
 	Height int
 
-	Parent record
+	Parent Record
 
 	ChildBucket location
-	Children map[string]record //maps child locations to indices in children slice
+	Children    map[string]Record //maps child locations to indices in children slice
 }
 
-var rootRecord *record = &record{
-	Loc: noNameSpace,
+func (n Node) Key() []byte {
+	return n.Record.Key()
 }
-
-func (n *node) Key() []byte {
-	return n.record.Key()
+func (n Node) KeyString() string {
+	return n.Record.KeyString()
 }
-func (n *node) KeyString() string {
-	return n.record.KeyString()
-}
-func (n *node) Update(u updateData) error {
-	return n.record.Update(u)
-}
-func (n *node) GetData() updater {
-	return n.record.GetData()
-}
-
-func joinParentAndChild (parent *node, child *node, metaData updater) {
-	parent.Children[child.Loc.KeyString()] = record{
-		Loc: child.Loc,
-		Data: metaData,
-	}
-	child.Parent = record{
-		Loc: parent.Loc,
-		Data: metaData,
-	}
-}
-
-//Updates the parent metaData for the child Node and the child metaData for the parent Node.  
-func updateConnectionMeta (parent *node, child *node, u updateData) error {
-
-	err := child.Parent.Update(u)
-
+func (n Node) Update(u updateData) (Record, error) {
+	r, err := n.Record.Update(u)
+	n.Record = r
 	if err != nil {
-		fmt.Println("error getting updating child node's parent", err)
+		return n.Record, err
+	}
+	return n.Record, nil
+}
+func (n Node) GetData() updater {
+	return n.Record.GetData()
+}
+
+func joinParentAndChild(parent Node, child Node, metaData updater) (Node, Node) {
+	parent.Children[child.Loc.KeyString()] = Record{
+		Loc:  child.Loc,
+		Data: metaData,
+	}
+	child.Parent = Record{
+		Loc:  parent.Loc,
+		Data: metaData,
+	}
+	return parent, child
+}
+
+//Updates the parent metaData for the child Node and the child metaData for the parent Node.
+func updateConnectionMeta(parent Node, child Node, u updateData) error {
+
+	parentMeta, err := child.Parent.Update(u)
+	child.Parent = parentMeta
+	if err != nil {
+		fmt.Println("error getting updating child Node's parent", err)
 		return err
 	}
 
-	parent.Children[child.KeyString()] = child.Parent
+	childMeta := parent.Children[child.KeyString()]
+
+	childMeta.Data = parentMeta.Data
+
+	parent.Children[child.KeyString()] = childMeta
 
 	if err != nil {
-		fmt.Println("error getting updating child node's parent", err)
+		fmt.Println("error getting updating child Node's parent", err)
 		return err
 	}
 
 	return err
 }
 
-//Creates a node whose children will be in the same namespace as this branch.
-func makeBranch (parent *node, metaData updater, data updater) (*node, error) {
-
+//Creates a Node whose children will be in the same namespace as this branch.
+func makeBranch(parent Node, metaData updater, data updater) (Node, error) {
+	var newBranch Node
 	bucket := parent.ChildBucket
 
 	loc, err := bucket.getNewLoc()
 
 	if err != nil {
 		fmt.Println("error getting new location", err)
-		return nil, err
+		return newBranch, err
 	}
 
-	newBranch := &node{
-		record: &record{
-			Loc: loc,
+	newBranch = Node{
+		Record: Record{
+			Loc:  loc,
 			Data: data,
 		},
-		Height: parent.Height + 1,
+		Height:      parent.Height + 1,
 		ChildBucket: bucket,
-		Children: make(map[string]record),
+		Children:    make(map[string]Record),
 	}
 
-	joinParentAndChild(parent, newBranch, metaData)
+	parent, newBranch = joinParentAndChild(parent, newBranch, metaData)
 
 	return newBranch, err
 }
 
-//creates a branch of the parent node who's children will be in a different
+//creates a branch of the parent Node who's children will be in a different
 //namespace than the new branch
-func makeTree (parent *node, metaData updater, data updater) (*node, error) {
+func makeTree(parent Node, metaData updater, data updater) (Node, error) {
 	newTree, err := makeBranch(parent, metaData, data)
 
 	if err != nil {
 		fmt.Println("Error creating template branch: ", err)
-		return nil, err
+		return newTree, err
 	}
 
-	newTree.ChildBucket = newTree.record.Loc
+	newTree.ChildBucket = newTree.Record.Loc
 
 	return newTree, nil
 }
 
-func (n *node) IsTree() bool {
+func (n Node) IsTree() bool {
 	//branch nodes put their children in the same bucket that they are in while
 	//trees put their children in a different bucket (currently tree children
-	//have their namespace set to the id of the tree node, but this may change
+	//have their namespace set to the id of the tree Node, but this may change
 	//in the future when I start optimizing for sequential reads through trees)
-	return !n.record.Loc.getBucketLocation().equals(n.ChildBucket)
+	return !n.Record.Loc.getBucketLocation().equals(n.ChildBucket)
 }
 
-//creates a tree at height 0
-func makeForest (root *node, metaData updater, data updater) (*node, error) {
+//creates a tree at height 0 attached to the root.  root should be the root of the db.  You could,
+//but shouldn't, pass any other Node to this function
+func makeForest(root Node, metaData updater, data updater) (Node, error) {
 	newForest, err := makeTree(root, metaData, data)
-	
+
 	if err != nil {
 		fmt.Println("Error creating template tree: ", err)
-		return nil, err
+		return newForest, err
 	}
 
 	newForest.Height = 0
-	
+
 	return newForest, nil
 }
 
-
-func makeRoot () *node {
-	return &node{
-		record: &record{
-			Loc: noNameSpace,
-		},
-		ChildBucket: noNameSpace,
-		Children: make(map[string]record),
-	}
-}
-
-func (n *node) IsForest() bool {
+func (n Node) IsForest() bool {
 	//the only special characteristic of a forest is that it's Height is 0.
 	//it's worth noting that this will return true for the root as well as
 	//forests.
 	return n.Height == 0 && n.IsTree()
 }
 
-//serializes the node into a gob and returns it as a byte slice
-func (n *node) serialize () ([]byte, error) {
-	gobble := new(bytes.Buffer)
-	enc := gob.NewEncoder(gobble)
-	err := enc.Encode(n)
-
-	if err != nil {
-		fmt.Println("SERIALIZATION ERROR: ", err)
-		return []byte{}, err
+func makeRoot() Node {
+	return Node{
+		Record: Record{
+			Loc: noNameSpace,
+		},
+		ChildBucket: noNameSpace,
+		Children:    make(map[string]Record),
 	}
-
-	return gobble.Bytes(), nil
-}
-
-//fills the record with deserialized data from the passed in gob
-func (n *node) deserialize(value []byte) error {
-	gobble := bytes.NewBuffer(value)
-	dec := gob.NewDecoder(gobble)
-	err := dec.Decode(n)
-	
-	if err != nil {
-		fmt.Println("DESERIALIZATION ERROR: ", err)
-		return err
-	}
-
-	return nil
-}
-
-//registers required types with gob.  Any named types contained in a 
-//record's data property must also be registered before serializing or 
-//deserializing to or from the db.
-func init () {
-	gob.Register(location{})
-	gob.Register(record{})
-	gob.Register(node{})
 }
--- tree_test.go
+++ tree_test.go
@@ -3,257 +3,361 @@ package levTree
 import (
 	"testing"
 	"fmt"
+	"encoding/gob"
 )
 
-func TestNewTree (t *testing.T) {
-	tree, err := NewTree(1, 2)
-	if err != nil {
-		t.Error("GUUID ERROR", err)
+type mockUpdateable int
+
+func init() {
+	var emptyMock mockUpdateable
+	gob.Register(emptyMock)
+	// gob.Register(location{})
+	// gob.Register(Record{})
+	// gob.Register(Node{})
+}
+
+func (number mockUpdateable) Update(u updateData) (updater, error) {
+	number += u.(mockUpdateable)
+	return number, nil
+}
+
+func testParentChildRel(t *testing.T, parent Node, child Node) {
+	if !parent.Record.Loc.equals(child.Parent.Loc) {
+		t.Error("CHILD'S PARENT LOC IS NOT THE PARENT'S LOC")
 	}
-	if tree.Data != 2 {
-		t.Error("TREE DATA WRONG")
+	if !child.Record.Loc.equals(parent.Children[child.KeyString()].Loc) {
+		t.Error("PARENT'S CHILD LOC IS NOT CHILD'S LOC")
 	}
-	if forest.Loc.equals(tree.Loc) {
-		t.Error("TREE LOC WRONG")
+	if parent.Children[child.KeyString()].Data != child.Parent.Data {
+		t.Error("PARENT'S AND CHILD'S METADATA ARE NOT EQUAL")
 	}
-	if tree.Parent.Data != 1 {
-		t.Error("TREE PARENT DATA WRONG")
-	}
-	if !(forest.Loc.equals(tree.Parent.Loc)) {
-		t.Error("TREE PARENT LOC ERROR")
-	}
-	if tree.Height != 0 {
-		t.Error("TREE HEIGHT WRONG")
-	}
-	if len(tree.Children) != 0 {
-		t.Error("NEW TREE HAS CHILDREN")
-	}
-	if len(forest.Children) != 1 {
-		t.Error("FOREST HAS WRONG NUMBER OF CHILDREN")
-	}
-	if forest.Children[0].Data != tree.Parent.Data {
-		t.Error("FOREST CHILD DATA WRONG")
-	}
-	if !(forest.Children[0].Loc.equals(tree.Loc)) {
-		t.Error("FOREST CHILD LOC WRONG")
+	if !parent.ChildBucket.equals(child.Loc.getBucketLocation()) {
+		t.Error("This Node was put in the wrong bucket!")
 	}
 }
 
-func TestNewChild (t *testing.T) {
-	tree, err := NewTree(1,2)
-	if err != nil {
-		t.Error("GUUID ERROR", err)
+func testNode(t *testing.T, parent Node, n Node, metaData updater, data updater) {
+	if d := n.Data; d != data {
+		t.Error("NODE HAS WRONG DATA: ", d)
+	}
+	if len(n.Children) != 0 {
+		t.Error("NODE SHOULD NOT HAVE CHILDREN: ", len(n.Children))
+	}
+	if len(n.Key()) != len(parent.ChildBucket.Key())+16 {
+		t.Error("NODE KEY SHOULD BE A GUUID: ", len(n.Key()))
+	}
+	if d := n.Parent.Data; d != metaData {
+		t.Error("NODE PARENT HAS WRONG META DATA: ", d)
 	}
 
-	n1, err := tree.NewChild(3, 4)
-	n2, err := tree.NewChild(5, 6)
-	if err != nil {
-		t.Error("GUUID ERROR", err)
-	}
-	if n1.Data != 4 {
-		t.Error("N1 DATA WRONG")
-	}
-	if tree.Loc.equals(n1.Loc) {
-		t.Error("N1 LOC WRONG")
-	}
-	if n1.Parent.Data != 3 {
-		t.Error("N1 PARENT DATA WRONG")
-	}
-	if !(tree.Loc.equals(n1.Parent.Loc)) {
-		t.Error("N1 PARENT LOC ERROR")
-	}
-	if n1.Height != 1 {
-		t.Error("N1 HEIGHT WRONG")
-	}
-	if len(n1.Children) != 0 {
-		t.Error("N1 HAS CHILDREN")
-	}
-	if len(tree.Children) != 2 {
-		t.Error("Tree HAS WRONG NUMBER OF CHILDREN")
-	}
-	if tree.Children[0].Data != n1.Parent.Data {
-		t.Error("TREE CHILD DATA WRONG")
-	}
-	if !(tree.Children[0].Loc.equals(n1.Loc)) {
-		t.Error("TREE CHILD LOC WRONG")
-	}
+	testParentChildRel(t, parent, n)
+}
 
-	if err != nil {
-		t.Error("GUUID ERROR", err)
+func testBranch(t *testing.T, parent Node, branch Node, metaData updater, data updater) {
+	testNode(t, parent, branch, metaData, data)
+	if branch.Height != parent.Height+1 {
+		t.Error("branch should be at height + 1")
 	}
-	if n2.Data != 6 {
-		t.Error("N2 DATA WRONG")
-	}
-	if tree.Loc.equals(n2.Loc) {
-		t.Error("N2 LOC WRONG")
-	}
-	if n2.Parent.Data != 5 {
-		t.Error("N2 PARENT DATA WRONG")
-	}
-	if !(tree.Loc.equals(n2.Parent.Loc)) {
-		t.Error("N2 PARENT LOC ERROR")
-	}
-	if n2.Height != 1 {
-		t.Error("N2 HEIGHT WRONG")
-	}
-	if len(n2.Children) != 0 {
-		t.Error("N2 HAS CHILDREN")
-	}
-	if tree.Children[1].Data != n2.Parent.Data {
-		t.Error("TREE CHILD DATA WRONG")
-	}
-	if !(tree.Children[1].Loc.equals(n2.Loc)) {
-		t.Error("TREE CHILD LOC WRONG")
-	}
-
-	n11, err := n1.NewChild(7, 8)
-	if err != nil {
-		t.Error("GUUID ERROR", err)
-	}
-	if n11.Data != 8 {
-		t.Error("N11 DATA WRONG")
-	}
-	if n1.Loc.equals(n11.Loc) {
-		t.Error("N11 LOC WRONG")
-	}
-	if n11.Parent.Data != 7 {
-		t.Error("N11 PARENT DATA WRONG")
-	}
-	if !(n1.Loc.equals(n11.Parent.Loc)) {
-		t.Error("N11 PARENT LOC ERROR")
-	}
-	if n11.Height != 2 {
-		t.Error("N11 HEIGHT WRONG")
-	}
-	if len(n11.Children) != 0 {
-		t.Error("N11 HAS CHILDREN")
-	}
-	if len(n1.Children) != 1 {
-		t.Error("N1 HAS WRONG NUMBER OF CHILDREN")
-	}
-	if n1.Children[0].Data != n11.Parent.Data {
-		t.Error("N1 CHILD DATA WRONG")
-	}
-	if !(n1.Children[0].Loc.equals(n11.Loc)) {
-		t.Error("N1 CHILD LOC WRONG")
+	if !branch.ChildBucket.equals(parent.ChildBucket) {
+		t.Error("branch will put children in wrong bucket!")
 	}
 }
 
-//tests if two nodes contain the same data.  If you're checking if the nodes
-//describe the same place in memory, then compare their locations with
-//location.equals(location).
-//data must be ==able
+func testTree(t *testing.T, parent Node, tree Node, metaData updater, data updater) {
+	testNode(t, parent, tree, metaData, data)
+	if tree.Height != parent.Height+1 {
+		t.Error("tree should be at height + 1: ")
+	}
+	if tree.ChildBucket.equals(parent.Loc) {
+		t.Error("tree will put children in wrong bucket!")
+	}
+}
 
-func testNodeEquality (n1, n2 *node) bool {
+func testForest(t *testing.T, root Node, forest Node, metaData updater, data updater) {
+	testNode(t, root, forest, metaData, data)
+	if forest.Height != 0 {
+		t.Error("forest should be at height + 1: ")
+	}
+	if forest.ChildBucket.equals(root.Loc) {
+		t.Error("forest will put children in wrong bucket!")
+	}
+}
+
+func TestMakeForest(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	// fmt.Println(t, root, forest, metaData, data)
+	testForest(t, root, forest, metaData, data)
+}
+
+func TestMakeTwoForests(t *testing.T) {
+	metaData1 := mockUpdateable(1)
+	data1 := mockUpdateable(2)
+	metaData2 := mockUpdateable(3)
+	data2 := mockUpdateable(4)
+
+	root := makeRoot()
+
+	forest1, err := makeForest(root, metaData1, data1)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	forest2, err := makeForest(root, metaData2, data2)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+
+	testForest(t, root, forest1, metaData1, data1)
+	testForest(t, root, forest2, metaData2, data2)
+
+}
+
+func TestMakeTree(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataTree := mockUpdateable(3)
+	dataTree := mockUpdateable(4)
+	tree, err := makeTree(forest, metaDataTree, dataTree)
+	if err != nil {
+		t.Error("ERROR MAKING TREE: ", err)
+	}
+	testTree(t, forest, tree, metaDataTree, dataTree)
+}
+
+func TestMakeTwoTrees(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataTree1 := mockUpdateable(3)
+	dataTree1 := mockUpdateable(4)
+	tree1, err := makeTree(forest, metaDataTree1, dataTree1)
+	if err != nil {
+		t.Error("ERROR MAKING TREE: ", err)
+	}
+	metaDataTree2 := mockUpdateable(5)
+	dataTree2 := mockUpdateable(6)
+	tree2, err := makeTree(forest, metaDataTree2, dataTree2)
+	if err != nil {
+		t.Error("ERROR MAKING TREE: ", err)
+	}
+	testTree(t, forest, tree1, metaDataTree1, dataTree1)
+	testTree(t, forest, tree2, metaDataTree2, dataTree2)
+}
+
+func TestMakeTreeOnTree(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataTree1 := mockUpdateable(3)
+	dataTree1 := mockUpdateable(4)
+	tree1, err := makeTree(forest, metaDataTree1, dataTree1)
+	if err != nil {
+		t.Error("ERROR MAKING TREE: ", err)
+	}
+	metaDataTree2 := mockUpdateable(5)
+	dataTree2 := mockUpdateable(6)
+	tree2, err := makeTree(tree1, metaDataTree2, dataTree2)
+	if err != nil {
+		t.Error("ERROR MAKING TREE: ", err)
+	}
+	testTree(t, tree1, tree2, metaDataTree2, dataTree2)
+}
+
+func TestMakeBranch(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataBranch := mockUpdateable(3)
+	dataBranch := mockUpdateable(4)
+	branch, err := makeBranch(forest, metaDataBranch, dataBranch)
+	if err != nil {
+		t.Error("ERROR MAKING BRANCH: ", err)
+	}
+	testBranch(t, forest, branch, metaDataBranch, dataBranch)
+}
+
+func TestMakeTwoBranches(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataBranch1 := mockUpdateable(3)
+	dataBranch1 := mockUpdateable(4)
+	branch1, err := makeBranch(forest, metaDataBranch1, dataBranch1)
+	if err != nil {
+		t.Error("ERROR MAKING BRANCH: ", err)
+	}
+	metaDataBranch2 := mockUpdateable(5)
+	dataBranch2 := mockUpdateable(6)
+	branch2, err := makeBranch(forest, metaDataBranch2, dataBranch2)
+	if err != nil {
+		t.Error("ERROR MAKING BRANCH: ", err)
+	}
+	testBranch(t, forest, branch2, metaDataBranch2, dataBranch2)
+	testBranch(t, forest, branch1, metaDataBranch1, dataBranch1)
+}
+
+func TestMakeBranchOnTree(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataTree := mockUpdateable(3)
+	dataTree := mockUpdateable(4)
+	tree, err := makeTree(forest, metaDataTree, dataTree)
+	if err != nil {
+		t.Error("ERROR MAKING TREE: ", err)
+	}
+	metaDatabranch := mockUpdateable(5)
+	databranch := mockUpdateable(6)
+	branch, err := makeBranch(tree, metaDatabranch, databranch)
+	if err != nil {
+		t.Error("ERROR MAKING BRANCH: ", err)
+	}
+	testBranch(t, tree, branch, metaDatabranch, databranch)
+}
+
+func TestMakeBranchOnBranch(t *testing.T) {
+	metaData := mockUpdateable(1)
+	data := mockUpdateable(2)
+	root := makeRoot()
+	forest, err := makeForest(root, metaData, data)
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	metaDataBranch1 := mockUpdateable(3)
+	dataBranch1 := mockUpdateable(4)
+	branch1, err := makeBranch(forest, metaDataBranch1, dataBranch1)
+	if err != nil {
+		t.Error("ERROR MAKING BRANCH: ", err)
+	}
+	metaDataBranch2 := mockUpdateable(3)
+	dataBranch2 := mockUpdateable(4)
+	branch2, err := makeBranch(branch1, metaDataBranch2, dataBranch2)
+	if err != nil {
+		t.Error("ERROR MAKING BRANCH: ", err)
+	}
+	testBranch(t, branch1, branch2, metaDataBranch2, dataBranch2)
+}
+
+
+func testNodeEquality (n1 Node, n2 Node) bool {
 	tests := []bool{
-		n1.Data == n2.Data,
-		n1.Loc.equals(n2.Loc),
+		n1.Record.Data == n2.Record.Data,
+		n1.Record.Loc.equals(n2.Record.Loc),
 		n1.Height == n2.Height,
 		testRecordEquality(n1.Parent, n2.Parent),
 		testRecordListEquality(n1.Children, n2.Children),
-		testMapEquality(n1.ChildrenMap, n2.ChildrenMap),
+		// testMapEquality(n1.ChildrenMap, n2.ChildrenMap),
 	}
-	for _, v := range tests {
+	for i, v := range tests {
 		if !v {
+			fmt.Println("FAILED NODE EQUALITY TEST AT INDEX ", i)
 			return false
 		}
 	}
 	return true
 }
-func testRecordEquality(r1, r2 record) bool {
+func testRecordEquality(r1, r2 Record) bool {
 	return r1.Data == r2.Data && r1.Loc.equals(r2.Loc)
 }
-func testRecordListEquality(rs1, rs2 []record) bool {
+func testRecordListEquality(rs1, rs2 map[string]Record) bool {
 	if len(rs1) != len(rs2) {
+		fmt.Println("FAILED LIST EQUALITY")
 		return false
 	}
 	for i, v := range rs1 {
 		if !testRecordEquality(v, rs2[i]) {
-			return false
-		}
-	}
-	return true
-}
-func testMapEquality(m1, m2 map[string]int) bool {
-	if len(m1) != len(m2) {
-		return false
-	}
-	for i, v := range m1 {
-		if v != m2[i] {
+			fmt.Println("FAILED RECORD EQUALITY")
+			fmt.Println("key:", i)
 			return false
 		}
 	}
 	return true
 }
 
-func serializeDeserializeTest (t *testing.T, n *node) {
-	gobble, err := n.serialize()
+func serializeDeserializeTest (t *testing.T, n Node) {
+	gobble, err := serialize(n)
 	if err != nil {
-		fmt.Println("SERIALIZE ERROR")
+		t.Error("SERIALIZE ERROR")
 	}
-	var newNode *node = &node{}
-	err = newNode.deserialize(gobble)
+	newNode, err := deserialize(gobble)
 	if err != nil {
-		fmt.Println("DESERIALIZE ERROR")
+		t.Error("DESERIALIZE ERROR", err)
 	}
 	if !testNodeEquality(n, newNode) {
+		fmt.Println("ORIGINAL: ", n)
+		fmt.Println("NEW: ", newNode)
 		t.Error("DESERIALIZE DID NOT RETURN SERIALIZED NODE")
 	}
 }
 
+func convertNumToUpdater(x int) updater {
+	u := mockUpdateable(x)
+	return u
+}
+
 func TestSerializeDeSerialize (t *testing.T) {
-	tree, err := NewTree(1, 2)
+	root := makeRoot()
+	forest, err := makeForest(root, convertNumToUpdater(-1), convertNumToUpdater(0))
+	if err != nil {
+		t.Error("ERROR MAKING FOREST: ", err)
+	}
+	tree, err := makeTree(forest, convertNumToUpdater(1), convertNumToUpdater(2))
 	if err != nil {
 		t.Error("GUUID ERROR", err)
 	}
-	n1, err := tree.NewChild(3,4)
+	n1, err := makeBranch(tree, convertNumToUpdater(3), convertNumToUpdater(4))
 	if err != nil {
 		t.Error("GUUID ERROR", err)
 	}
-	n2, err := tree.NewChild(5,6)
+	n2, err := makeBranch(tree, convertNumToUpdater(5), convertNumToUpdater(6))
 	if err != nil {
 		t.Error("GUUID ERROR", err)
 	}
-	n11, err := n1.NewChild(7,8)
+	n11, err := makeBranch(n1, convertNumToUpdater(7), convertNumToUpdater(8))
 	if err != nil {
 		t.Error("GUUID ERROR", err)
 	}
-	n12, err := n1.NewChild(9,10)
+	n12, err := makeBranch(n1, convertNumToUpdater(9), convertNumToUpdater(10))
 	if err != nil {
 		t.Error("GUUID ERROR", err)
 	}
 	if !testNodeEquality(n1, n1) {
 		t.Error("NODE EQUALITY FUNCTION IS BROKEN")
 	}
+	// fmt.Println("derp", *tree)
 	serializeDeserializeTest(t, tree)
 	serializeDeserializeTest(t, n1)
 	serializeDeserializeTest(t, n2)
 	serializeDeserializeTest(t, n11)
 	serializeDeserializeTest(t, n12)
-}
-
-//this tests the above functions but for a specific namespace placed in
-//forest.
-func TestInitForest (t *testing.T) {
-	fmt.Println("InitForest Tests Basic")
-	InitForest([]byte("derp"), 1)
-	if forest.Data != 1 {
-		t.Error("FOREST DATA NOT SET")
-	}
-	if forest.Loc.KeyString() != "derp" {
-		t.Error("FOREST NOT NAMESPACED")
-	}
-	TestNewTree(t)
-	TestNewChild(t)
-
-	fmt.Println("InitForest Tests Empty Name Space")
-	InitForest([]byte{}, 2)
-	if forest.Data != 2 {
-		t.Error("FOREST DATA NOT SET")
-	}
-	if forest.Loc.KeyString() != "" {
-		t.Error("FOREST NOT NAMESPACED")
-	}
-	TestNewTree(t)
-	TestNewChild(t)
 }
